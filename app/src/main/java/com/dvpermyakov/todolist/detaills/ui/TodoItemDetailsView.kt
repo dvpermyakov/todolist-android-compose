636bf9b26031cee166fac7720c9b63a9a924a059
dvpermyakov <dvpermyakov1@gmail.com>
2020-11-26 11:37:11 +0400

    improve TodoItemDetailsView.kt

--- app/src/main/java/com/dvpermyakov/todolist/detaills/ui/TodoItemDetailsView.kt
+++ app/src/main/java/com/dvpermyakov/todolist/detaills/ui/TodoItemDetailsView.kt
@@ -1,17 +1,27 @@
 package com.dvpermyakov.todolist.detaills.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
 import com.dvpermyakov.todolist.R
 import com.dvpermyakov.todolist.list.domain.TodoItem
 
 @Composable
 fun TodoItemDetails(
-    item: TodoItem,
-    onBack: () -> Unit
+    onBack: () -> Unit,
+    item: TodoItem
 ) {
     Scaffold(
         topBar = {
@@ -27,5 +37,39 @@ fun TodoItemDetails(
             )
         }
     ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            Text(
+                text = item.title
+            )
+            Text(
+                text = item.description,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+            Image(
+                asset = imageResource(id = R.drawable.ic_cat),
+                modifier = Modifier
+                    .width(256.dp)
+                    .height(256.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(top = 8.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewTodoItemDetails() {
+    TodoItemDetails(
+        {},
+        TodoItem(
+            image = 0,
+            title = "Need to improve android article about Compose",
+            description = "Need not only improve, but rewrite some code as well. Good to do it as soon as possible"
+        )
+    )
 }
